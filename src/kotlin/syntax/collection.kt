dab2a73a0b8999e8841e9ecd384d6a85e7372b9b
sunjinbo <sunjinbo@360.cn>
2020-09-12 23:23:56 +0800

    数组

--- src/kotlin/syntax/collection.kt
+++ src/kotlin/syntax/collection.kt
@@ -1,6 +1,21 @@
 package kotlin.syntax
 
 // 数组
+val arr1 = arrayOf(1, 2, 3)
+val arr2 = Array(3) { it -> it.inc()}
+
+val intArr: IntArray = intArrayOf(1,2,3)
+val longArr: LongArray = longArrayOf(1L,2L,3L)
+val floatArr: FloatArray = floatArrayOf(1.0f,2.0f,3.0f)
+val doubleArr: DoubleArray = doubleArrayOf(1.0,2.02,3.03333)
+val booleanArr: BooleanArray = booleanArrayOf(false,true,false)
+
+val fixedSizeArr = arrayOfNulls<Int>(6)  // 创建指定长度的数组且无元素
+val empty = emptyArray<Int>()
+val intArray = IntArray(3)
+
+// 二维数组
+val arr = Array(3){IntArray(3)}
 
 // 列表
 
