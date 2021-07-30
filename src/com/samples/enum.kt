c1f4045a5a332f0189c3c5e337ca86c6df3afc1c
sunjinbo <sunjinbo@360.cn>
2021-07-30 17:47:02 +0800

    enum

--- src/com/samples/enum.kt
+++ src/com/samples/enum.kt
@@ -1,3 +1,37 @@
 package com.samples
 
 // 该文件主要用于学习kotlin枚举
+
+enum class Direction {
+    NORTH, SOUTH, WEST, EAST
+}
+
+enum class Color(val rgb: Int) {
+    RED(0xFF0000),
+    GREEN(0x00FF00),
+    BLUE(0x0000FF);
+
+    fun containsRed() = (this.rgb and 0xFF0000 != 0)
+}
+
+enum class ProtocolState {
+    WAITING {
+        override fun signal() = TALKING
+    },
+
+    TALKING {
+        override fun signal() = WAITING
+    };
+
+    abstract fun signal(): ProtocolState
+}
+
+
+// Main函数
+fun main() {
+    val red = Color.RED
+    println(red)
+    println(red.containsRed())
+    println(Color.BLUE.containsRed())
+    println(Color.GREEN.containsRed())
+}
