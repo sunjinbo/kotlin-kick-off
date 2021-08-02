bb6dfb9227d6fa641cf322cd73da2b4275648eb6
sunjinbo <sunjinbo@360.cn>
2021-08-02 19:26:12 +0800

    exception

--- src/com/samples/exception.kt
+++ src/com/samples/exception.kt
@@ -1,3 +1,20 @@
 package com.samples
 
 // 该文件主要用于学习kotlin异常处理
+
+// Main函数
+fun main() {
+    try {
+        // some code
+        var a = 0
+        var b = 10
+        var c = b / a
+        println("$b ÷ $a = $c")
+    } catch (e: Exception) {
+        // handler
+        println(e.message)
+    } finally {
+        // optional finally block
+        println("This program is over.")
+    }
+}
