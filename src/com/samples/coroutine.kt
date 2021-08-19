d697b12676cfb206a071b60591293b7f450ca5fc
sunjinbo <sunjinbo@360.cn>
2021-08-19 17:44:41 +0800

    coroutine

--- src/com/samples/coroutine.kt
+++ src/com/samples/coroutine.kt
@@ -1,10 +1,21 @@
 package com.samples
 
+import kotlin.coroutines.*
 import kotlin.coroutines.startCoroutine
+import kotlin.suspend
 
 // 该文件主要用于学习kotlin协程的使用
 
 // Main方法
 fun main() {
+    var continuation = suspend {
+        5
+    }.createCoroutine(object : Continuation<Int> {
+        override val context: CoroutineContext = EmptyCoroutineContext
 
+        override fun resumeWith(result: Result<Int>) {
+            println("Coroutine End: $result")
+        }
+    })
+    continuation.resume(Unit)
 }
